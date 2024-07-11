564b986eca1e2edaab3c59e01e3c0a5916628332
keiakihito <kkatsumi.git@gmail.com>
2024-07-10 18:11:06 -0700

    Add sample source code

---
+++ static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const element = document.getElementById('animate-me');
+    element.style.transition = 'transform 2s';
+    element.style.transform = 'translateX(100px)';
+});
